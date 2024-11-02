6881e232c2899b4ce269e44bee2fc7e456f4a199
Devloper-solaiman <devlopersolaiman@gmail.com>
2024-11-02 11:36:04 +0600

    Love program start

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <h1 className='text-center font-bold text-3xl mt-44'>Home Page</h1>
+    </>
+  );
+};
+
+export default Home;
